7d6c3c2a7564d91e86a5d3914f2af163146912d7
maximilionus <maximilionuss@gmail.com>
2025-04-11 23:38:12 +0300

    Initial commit, split from dotfiles repo

---
+++ patch.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+TOOLCHAIN="./bin"
+
+if [[ ! -d $TOOLCHAIN ]]; then
+    echo "Revanced toolchain not found!"
+    ./install.sh
+fi
+
+patch_tiktok() {
+    echo "- Patching TikTok"
+    echo
+
+    java -jar "$TOOLCHAIN/revanced-cli.jar" patch \
+        --patches "$TOOLCHAIN/patches.rvp" \
+        --enable "SIM spoof" \
+        "$1"
+
+    echo
+    echo "Done"
+}
+
+patch_spotify() {
+    echo "- Patching Spotify"
+    echo
+
+    java -jar "$TOOLCHAIN/revanced-cli.jar" patch \
+        --patches "$TOOLCHAIN/patches.rvp" \
+        --exclusive \
+        --enable "Spoof signature" \
+        "$1"
+
+    echo
+    echo "Done"
+}
+
+package_name=$( basename "$1" )
+case $package_name in
+    com.zhiliaoapp.musically*)
+        patch_tiktok "$1"
+        ;;
+    com.spotify.music*|Spotify*)
+        patch_spotify "$1"
+        ;;
+    *)
+        echo "No supported apk provided to script"
+        ;;
+esac
---
+++ remove.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+TOOLCHAIN="./bin"
+
+
+echo "Removing the toolchain..."
+rm -rfv "$TOOLCHAIN"
+echo "Done"
