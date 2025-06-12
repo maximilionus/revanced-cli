2496eb283a50bc9c6d1bdc1fa41badd7911d089e
maximilionus <maximilionuss@gmail.com>
2025-06-13 01:14:02 +0300

    Bump CLI and Patches versions

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ set -e
 TOOLCHAIN="./bin"
 
 declare -A files=(
-    ["revanced-cli.jar"]="https://github.com/ReVanced/revanced-cli/releases/download/v5.0.0/revanced-cli-5.0.0-all.jar"
-    ["patches.rvp"]="https://github.com/ReVanced/revanced-patches/releases/download/v5.14.0/patches-5.14.0.rvp"
+    ["revanced-cli.jar"]="https://github.com/ReVanced/revanced-cli/releases/download/v5.0.1/revanced-cli-5.0.1-all.jar"
+    ["patches.rvp"]="https://github.com/ReVanced/revanced-patches/releases/download/v5.27.0/patches-5.27.0.rvp"
 )
 
 
